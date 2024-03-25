9227ed01863aadc107cdc523e5c5a58fd1023f70
sorinupwork <sorin.upwork@gmail.com>
2024-03-25 22:16:45 +0100

    designing the product page

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -2,14 +2,33 @@
 
 import { trpc } from '@/app/_trpc/client';
 import UploadButton from './UploadButton';
-import { Ghost, MessageSquare, Plus, Trash } from 'lucide-react';
+import { Ghost, Loader2, MessageSquare, Plus, Trash } from 'lucide-react';
 import Skeleton from 'react-loading-skeleton';
 import Link from 'next/link';
 import { format } from 'date-fns';
 import { Button } from './ui/button';
+import { useState } from 'react';
 
 const Dashboard = () => {
+  const [currentlyDeletingFile, setCurrentlyDeletingFile] = useState<
+    string | null
+  >(null);
+
+  const utils = trpc.useContext();
+
   const { data: files, isLoading } = trpc.getUserFiles.useQuery();
+
+  const { mutate: deleteFile } = trpc.deleteFile.useMutation({
+    onSuccess: () => {
+      utils.getUserFiles.invalidate();
+    },
+    onMutate({ id }) {
+      setCurrentlyDeletingFile(id);
+    },
+    onSettled() {
+      setCurrentlyDeletingFile(null);
+    },
+  });
 
   return (
     <main className="mx-auto max-w-7xl md:p-10">
@@ -62,11 +81,20 @@ const Dashboard = () => {
                   </div>
 
                   <Button
+                    onClick={() =>
+                      deleteFile({
+                        id: file.id,
+                      })
+                    }
                     size={'sm'}
                     className="w-full"
                     variant={'destructive'}
                   >
-                    <Trash className="h-4 w-4" />
+                    {currentlyDeletingFile === file.id ? (
+                      <Loader2 className="h-4 w-4 animate-spin" />
+                    ) : (
+                      <Trash className="h-4 w-4" />
+                    )}
                   </Button>
                 </div>
               </li>
